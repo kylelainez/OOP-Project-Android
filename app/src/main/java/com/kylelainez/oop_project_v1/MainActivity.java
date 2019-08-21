9aa0e94a47669fcbbd5a98617083b0820c4cddfd
Kurt <kurtdariel@gmail.com>
2019-08-21 12:25:18 +0800

    Kurt Changes

--- app/src/main/java/com/kylelainez/oop_project_v1/MainActivity.java
+++ app/src/main/java/com/kylelainez/oop_project_v1/MainActivity.java
@@ -2,7 +2,7 @@ package com.kylelainez.oop_project_v1;
 
 import android.app.Dialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
